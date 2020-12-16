9b63f3035ae5c4095cfa293fec46b4bea31dcd60
RiichD <47849926+RiichD@users.noreply.github.com>
2020-12-16 14:55:28 +0100

    Add files via upload

--- Mini-Projet2/API_Web.py
+++ Mini-Projet2/API_Web.py
@@ -24,16 +24,16 @@ def publications(id):
 	"""
 	res = {}
 	nb_pub = 0
-	if id <= 0:
+	if id <= 0: #id ne peut pas être inférieur à 1
 		redirect("/error/403/" + f"{id} is not between 1 or more and the id must be an integer, try again.")
 	for child in root:
 		if nb_pub == id-1:
-			n = 0
+			n = 0 #Entier permettant de différencier les clés identiques
 			for i in range(len(child)):
-				if child[i].tag in res: #Gère les cas lorsqu'il y a plusieurs clés similaires
+				if child[i].tag in res: #Cas lorsqu'il y a plusieurs clés identiques
 					res[child[i].tag + str(n)] = child[i].text
 					n += 1
-				else:
+				else: #Cas lorsqu'il n'y a qu'une seule clé
 					res[child[i].tag] = child[i].text
 					n = 0
 			return res
@@ -60,7 +60,7 @@ def publications():
 	for child in root:
 		tmp = {} #Stocke les informations temporairement
 		isNull = True
-		n = 0
+		n = 0 #Entier permettant de différencier les clés identiques
 		for i in range(len(child)):
 			if child[i].text is not None:
 				if child[i].tag in tmp:
@@ -70,8 +70,8 @@ def publications():
 					tmp[child[i].tag] = child[i].text
 					n = 0
 				isNull = False
-		if not isNull:
-			res.append(tmp) #Formatage
+		if not isNull: #S'il y a des données, on ajoute tmp dans la liste res
+			res.append(tmp)
 		if len(res) == limit:
 			return {'data':res}
 	redirect("/error/404/"+"Page not found")
@@ -120,14 +120,44 @@ def auth_pub(name):
 	La fonction retourne un dictionnaire de la liste des publications de name, les données sont stockées dans un dictionnaire.
 	"""
 	res = []
+	i = 0 #Iterator
+	min = 0 #Indice à partir duquel on veut récupérer les informations
+	isCounting = False
+	
+	#Récupération des paramètres et vérification de ces paramètres
+	param_start = request.query.get("start")
+	param_count = request.query.get("count")
+	param_order = request.query.get("order")
+	if param_start is None:
+		print("Start parameter not enabled\n")
+	else:
+		if not param_start.isnumeric() or int(param_start) < 0:
+			print("Start parameter not enabled: Value below 0\n")
+			redirect("/error/403/" + "An error occurred: start value is below 0! Must be a number, integer between 0 or more")
+		else:
+			min = int(param_start)
+			print(f"Start parameter enabled, starting at {min}\n")
+	
+	if param_count is None:
+		print("Count parameter not enabled\n")
+	else:
+		if not param_count.isnumeric() or int(param_count) < 0:
+			print("Count parameter not enabled: Value below 0\n")
+			redirect("/error/403/" + "An error occurred: count value is below 0! Must be a number, integer between 0 or more")
+		else:
+			print(f"Count parameter enabled, retrieving {param_count} values\n")
+			isCounting = True
+	max = 100+min
+	
+	#Rercherche des informations souhaitées
 	try:
 		for child in root:
-			pub = {}
+			pub = {} #Dictionnaire contenant les informations
 			isAuthor = False
 			n = 0
 			for data in child:
 				if data.text is not None:
-					if data.tag in pub: #Gère les cas lorsqu'il y a plusieurs clés similaires
+					if data.tag in pub: #Cas lorsqu'il y a plusieurs clés identiques
 						pub[data.tag + str(n)] = data.text
 						n += 1
 					else:
@@ -135,13 +165,52 @@ def auth_pub(name):
 						n = 0
 					if not isAuthor and 'author' == data.tag and name == data.text:
 						isAuthor = True
-			if isAuthor:
-				print(f"{name} is an author of {pub}\n")
-				res.append(pub)
+			if isAuthor: #L'auteur a été trouvé dans cette publication, on l'ajoute dans la liste si les conditions le permet
+				if min > i:
+					i += 1
+				elif isCounting and len(res) >= int(param_count):
+					break
+				elif len(res) < max-min:
+					print(f"{name} is an author of {pub}\n")
+					res.append(pub)
 	except Exception as exc: #Capture les erreurs inattendues
 		print("An error occurred: ", exc)
 		redirect("/error/403/" + f"An error occurred: " + str(exc))
+	
+	#Gère les clés et l'ordre
+	keyExists = False
+	isInteger = True
+	if param_order:
+		for r in res:
+			if param_order in r:
+				print(f'{param_order} exists\n')
+				keyExists = True
+				break
+	
+	if keyExists:
+		tmp = []
+		for r in res: #Boucle permettant de vérifier si la clé existe et si c'est un entier
+			if param_order in r:
+				tmp.append(r)
+				if isInteger:
+					try:
+						if int(r[param_order]):
+							continue
+					except ValueError as exc: #Ce n'est pas entier, l'ordre dépend des caractères
+						isInteger = False
+						print(f'{param_order} is not an integer\n')
+		if len(tmp) > 0:
+			if isInteger:
+				print(f"{param_order} is an integer\n")
+				res_ordered=sorted(tmp, key=lambda x: int(x[param_order]))
+			else:
+				res_ordered=sorted(tmp, key=lambda x: x[param_order])
+			for r in res:
+				if r not in res_ordered:
+					res_ordered.append(r) #Les cas qui ne peuvent pas être ordonné sont à la fin de la liste
+			return {'data':res_ordered}
 	return {'data':res}
+
 
 @route("/authors/<name>/coauthors")
 def auth_pub(name):
@@ -150,6 +219,36 @@ def auth_pub(name):
 	Les données sont stockées sous forme de dictionnaire.
 	"""
 	res = [] #Liste des coauteurs
+	i = 0 #Iterator
+	min = 0 #Indice à partir duquel on veut récupérer les informations
+	isCounting = False
+	
+	#Recherche des paramètres et vérifications de ces paramètres
+	param_start = request.query.get("start")
+	param_count = request.query.get("count")
+	param_order = request.query.get("order")
+	if param_start is None:
+		print("Start parameter not enabled\n")
+	else:
+		if not param_start.isnumeric() or int(param_start) < 0:
+			print("Start parameter not enabled: Value below 0\n")
+			redirect("/error/403/" + "An error occurred: start value is below 0! Must be a number, integer between 0 or more")
+		else:
+			min = int(param_start)
+			print(f"Start parameter enabled, starting at {min}\n")
+	
+	if param_count is None:
+		print("Count parameter not enabled\n")
+	else:
+		if not param_count.isnumeric() or int(param_count) < 0:
+			print("Count parameter not enabled: Value below 0\n")
+			redirect("/error/403/" + "An error occurred: count value is below 0! Must be a number, integer between 0 or more")
+		else:
+			print(f"Count parameter enabled, retrieving {param_count} values\n")
+			isCounting = True
+	max = 100+min
+	
+	#Boucle recherchant les informations souhaitées
 	try:
 		for child in root:
 			isAuthor = False
@@ -160,22 +259,30 @@ def auth_pub(name):
 					isAuthor = True
 					break
 			
-			if isAuthor:
-				for data in child: #Boucle recherchant les coauteurs de name
+			if isAuthor: #C'est un auteur de la publication, recherche des autres auteurs (coauteurs)
+				for data in child:
 					author = {}
 					if 'author' == data.tag and data.text is not None:
 						author[data.tag] = data.text
 						if name != data.text:
 							isCoauthor = True
-							if author not in res:
-								print(data.text+" is a coauthor")
-								res.append(author)
+							if author not in res: #Un auteur différent de name est trouvé
+								if min > i:
+									i += 1
+								elif isCounting and len(res) >= int(param_count):
+									break
+								elif len(res) < max-min:
+									print(data.text+" is a coauthor")
+									res.append(author)
 					else:
 						break
 	except Exception as exc: #Capture les erreurs inattendues
 		print("An error occurred: ", exc)
 		redirect("/error/403/" + f"An error occurred: " + str(exc))
 	print("Liste des coauteurs:\n", res)
+	if param_order == "author": #Il n'y a que la clé author dans la liste
+		res_ordered=sorted(res, key=lambda x: x["author"])
+		return {'data':res_ordered}
 	return {'data':res}
 
 @route("/search/authors/<searchString>")
@@ -186,10 +293,11 @@ def search_authors(searchString):
 	"""
 	res = []
 	i = 0 #Iterator
-	min = 0
+	min = 0 #Indice à partir duquel on veut récupérer les informations
 	isFull = False #Booléen qui indique si la liste d'auteurs a atteint le maximum
-	isCounting = False #Active le compteur cpt qui compte le nombre d'auteurs ajoutés dans la liste
+	isCounting = False
 	
+	#Recherche des paramètres et vérifications de ces paramètres
 	param_start = request.query.get("start")
 	param_count = request.query.get("count")
 	param_order = request.query.get("order")
@@ -228,7 +336,7 @@ def search_authors(searchString):
 							i += 1
 						elif  i >= max: #Le nombre d'auteurs a retourner en résultat est au maximum
 							isFull = True
-						elif isCounting and len(res) == int(param_count):
+						elif isCounting and len(res) >= int(param_count):
 							isFull = True
 						elif len(res) < max-min:
 							print(author)
@@ -256,11 +364,12 @@ def search_pub(searchString):
 	"""
 	res = [] #Liste des publications
 	i = 0 #Iterator
-	min = 0
+	min = 0 #Indice à partir duquel on veut récupérer les informations
 	filter_list = [] #Liste des filtres
 	isFilter = False
-	isCounting = False #Active le compteur cpt qui compte le nombre d'auteurs ajoutés dans la liste
+	isCounting = False
 	
+	#Recherche des paramètres et vérifications de ces paramètres
 	param_filter = request.query.get("filter")
 	param_start = request.query.get("start")
 	param_count = request.query.get("count")
@@ -309,17 +418,15 @@ def search_pub(searchString):
 	
 	try:
 		for child in root:
-			"""if min > i:
-				i += 1
-			elif (isCounting and len(res) >= cpt) or (len(res) >= max-min):
-				break
-			else:"""
 			cpt_filter = 0 #Compte le nombre de filtre
 			titleExists = False
 			dict_res = {} #Ne pas utiliser clear() pour vider!
 			n = 0
-			for data in child:
+			isAuthor = False
+			for data in child: #Recherche d'un titre contenant searchString
 				if data.text is not None:
+					if 'author' in data.tag: #S'il y a un auteur et non un editeur ou autre
+						isAuthor = True
 					if data.tag in dict_res: #Cas lorsqu'il a plusieurs clés similaires
 						dict_res[data.tag + str(n)] = data.text
 						n += 1
@@ -329,7 +436,8 @@ def search_pub(searchString):
 					if "title" == data.tag and searchString in data.text:
 						titleExists = True
 			
-			if titleExists and not isFilter and dict_res not in res: #Cas si le filtre n'est pas activé
+			#Cas si searchString est dans le titre
+			if isAuthor and titleExists and not isFilter and dict_res not in res: #Cas si le filtre n'est pas activé
 				if min > i:
 					i += 1
 				elif i >= max or (isCounting and len(res) >= int(param_count)) or len(res) >= max-min:
@@ -337,12 +445,12 @@ def search_pub(searchString):
 				else:
 					print(f"{dict_res} added with filters\n")
 					res.append(dict_res)
-			elif titleExists and isFilter and dict_res not in res:
+			elif isAuthor and titleExists and isFilter and dict_res not in res:
 				for data in child:
 					if data.text is not None:
 						for f in filter_list:
-							if f[0] == data.tag and f[1] in data.text:
-								cpt_filter += 1
+							if f[0] in data.tag and f[1] in data.text: #Comparaison de toutes les options dans le filtre
+								cpt_filter += 1 #Le compteur doit être égal au nombre de filtre présent dans la liste pour que ce soit une publication valable
 					
 				if isFilter and cpt_filter >= len(filter_list) and titleExists: #Cas si le filtre est activé et valable
 					if min > i:
